d5fbc8eb8fe3fc39622722c4d9b0d607188e871b
SultanKhassenov <154452152+SultanKhassenov@users.noreply.github.com>
2026-01-11 14:46:36 +0500

    PureDocumentPage change

--- src/pages/PureDocumentPage.tsx
+++ src/pages/PureDocumentPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import type { Order } from '../types';
 import OrderDocumentContent from '../components/OrderDocumentContent';
 import EmptyPage from './EmptyPage';
